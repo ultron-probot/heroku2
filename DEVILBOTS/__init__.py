e7aef410ed35154ef45535860f3bd7413e20daba
ultron-probot <smitjhn392@gmail.com>
2026-01-01 14:07:21 +0000

    new

---
+++ DEVILBOTS/__init__.py
@@ -0,0 +1,16 @@
+import pyromod.listen  # noqa
+from pyrogram import Client, filters
+from config import *
+
+app = Client(
+    name="PURVIBOTS",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+)
+
+filters.sudo = filters.create(
+    lambda _, __, m: bool(m.from_user and m.from_user.id in OWNER_ID),
+    "SudoFilter",
+)
